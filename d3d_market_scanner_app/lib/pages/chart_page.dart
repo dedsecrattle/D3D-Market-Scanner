9a28448802af0539c6018c3dbb70d8d879501813
Prabhat Kumar <itsprabxxx@gmail.com>
2023-06-23 01:58:20 +0530

    Added Live Interactive Chart
    Now Users can view Realtime Live Chart and also interact with different Currency pairs and Indicators

--- d3d_market_scanner_app/lib/pages/chart_page.dart
+++ d3d_market_scanner_app/lib/pages/chart_page.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:d3d_market_scanner_app/side-menu/side_menu_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class ChartPage extends StatefulWidget {
   const ChartPage({super.key});
@@ -9,14 +11,66 @@ class ChartPage extends StatefulWidget {
 }
 
 class _ChartPageState extends State<ChartPage> {
+  final String chartHtml = '''
+    <html>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      </head>
+      <body>
+        <!-- TradingView Widget BEGIN -->
+        <div class="tradingview-widget-container">
+          <div id="tradingview_61f61"></div>
+          <div class="tradingview-widget-copyright">
+            <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
+              <span class="blue-text">Track all markets on TradingView</span>
+            </a>
+          </div>
+          <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+          <script type="text/javascript">
+            new TradingView.widget(
+              {
+                "autosize": true,
+                "symbol": "EURUSD",
+                "interval": "D",
+                "timezone": "Etc/UTC",
+                "theme": "light",
+                "style": "1",
+                "locale": "en",
+                "toolbar_bg": "#f1f3f6",
+                "enable_publishing": false,
+                "allow_symbol_change": true,
+                "container_id": "tradingview_61f61"
+              }
+            );
+          </script>
+        </div>
+        <!-- TradingView Widget END -->
+      </body>
+    </html>
+  ''';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Charts'),
-          backgroundColor: Colors.pink,
-          leading: const SideMenuWidget(),
+      appBar: AppBar(
+        title: const Text('Live Forex Chart'),
+        backgroundColor: Colors.pink,
+        leading: const SideMenuWidget(),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: WebView(
+          initialUrl: '',
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController controller) {
+            controller.loadUrl(Uri.dataFromString(
+              chartHtml,
+              mimeType: 'text/html',
+              encoding: Encoding.getByName('utf-8'),
+            ).toString());
+          },
         ),
-        body: const Center(child: Text("Live Trading Charts Coming Soon !")));
+      ),
+    );
   }
 }
