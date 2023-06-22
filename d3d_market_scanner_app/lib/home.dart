9ea178d4b90a7946d87381f2b30338f5acf3b4b7
Prabhat Kumar <itsprabxxx@gmail.com>
2023-06-22 22:18:30 +0530

    Updated Home Page
    Added realtime Forex Market Hours and Trading session Information

--- d3d_market_scanner_app/lib/home.dart
+++ d3d_market_scanner_app/lib/home.dart
@@ -17,6 +17,106 @@ class _HomeState extends State<Home> {
         title: const Text('Home'),
         leading: const SideMenuWidget(),
       ),
+      body: Column(children: [
+        const SizedBox(
+          height: 20,
+        ),
+        const Center(
+          child: Text(
+            "Forex Market Around World",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: DataTable(
+              dataRowMinHeight: 25,
+              columnSpacing: MediaQuery.of(context).size.width / 2.5,
+              columns: const [
+                DataColumn(
+                    label: Text(
+                  'Session',
+                  style: TextStyle(fontSize: 18),
+                )),
+                DataColumn(
+                    label: Text(
+                  'Status',
+                  style: TextStyle(fontSize: 18),
+                )),
+              ],
+              rows: [
+                DataRow(cells: [
+                  const DataCell(Text(
+                    'London',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+                  DataCell(getStatus('london')),
+                ]),
+                DataRow(cells: [
+                  const DataCell(Text(
+                    'Tokyo',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+                  DataCell(getStatus('tokyo')),
+                ]),
+                DataRow(cells: [
+                  const DataCell(Text(
+                    'New York',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+                  DataCell(getStatus('newyork')),
+                ]),
+                DataRow(cells: [
+                  const DataCell(Text(
+                    'Sydney',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+                  DataCell(getStatus('sydney')),
+                ]),
+              ],
+            ),
+          ),
+        )
+      ]),
     );
+  }
+
+  Text getStatus(String session) {
+    DateTime datenow = DateTime.now().toUtc();
+    int hour = datenow.hour;
+    if (session == 'london') {
+      if (hour >= 8 && hour < 16) {
+        return const Text("Open",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green));
+      } else {
+        return const Text("Closed",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red));
+      }
+    } else if (session == "tokyo") {
+      if (hour == 24 || (hour >= 1 && hour < 9)) {
+        return const Text("Open",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green));
+      } else {
+        return const Text("Closed",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red));
+      }
+    } else if (session == 'newyork') {
+      if (hour >= 13 && hour < 22) {
+        return const Text("Open",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green));
+      } else {
+        return const Text("Closed",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red));
+      }
+    } else {
+      if ((hour >= 20 && hour < 24) || (hour >= 1 && hour < 6)) {
+        return const Text("Open",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green));
+      } else {
+        return const Text("Closed",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red));
+      }
+    }
   }
 }
